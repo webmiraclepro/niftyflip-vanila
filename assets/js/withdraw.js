90b2a28de5f3fc33b76e785320ee8d2b03152150
webmiraclepro <webmiraclepro@gmail.com>
2022-01-24 03:44:00 +0800

    vanila-update

--- assets/js/withdraw.js
+++ assets/js/withdraw.js
@@ -3,15 +3,15 @@ let openChatBox = document.getElementById("open_chatbox")
 let closeChatBox = document.getElementById("close_chatbox")
 let openSideBar = document.getElementById("btn_sidebar")
 let openMenu = document.getElementById("open_menu")
-let openModal = document.getElementById("view_modal")
-let closeModal = document.getElementById("close_modal")
+let closeCheckout = document.getElementById("close_checkout")
 
 // Component variable
 let chatBox = document.querySelector(".chat-box")
 let main = document.querySelector(".main")
 let sideBar = document.querySelector(".sidebar")
 let vMenu = document.querySelector(".v-menu")
-const modal = document.getElementsByClassName("modal-container")[0]
+let checkout = document.querySelector(".checkout")
+let withDrawPanel = document.querySelector(".withdraw-panel")
 
 // Chat send textarea auto resize function
 const tx = document.getElementsByTagName("textarea")
@@ -84,55 +84,8 @@ openMenu.addEventListener("click", () => {
   }
 })
 
-// Modal hide and show function
-openModal.addEventListener("click", () => {
-  // Clear chart
-  while (document.getElementById("chart_panel").hasChildNodes()) {
-    document
-      .getElementById("chart_panel")
-      .removeChild(document.getElementById("chart_panel").firstChild)
-  }
-
-  // Chart data get function
-  // create a data set on our data
-  var dataSet = anychart.data.set(getData())
-
-  // map data for the first series,
-  // take x from the zero column and value from the first column
-  var firstSeriesData = dataSet.mapAs({ x: 0, value: 1 })
-
-  // map data for the second series,
-  // take x from the zero column and value from the second column
-  var secondSeriesData = dataSet.mapAs({ x: 0, value: 2 })
-
-  // create a line chart
-  var chart = anychart.line()
-  chart.background().fill("transparent")
-
-  var xlabels = chart.xAxis().labels()
-  xlabels.enabled(false)
-
-  chart.xScale().mode("continuous")
-
-  // create the first series with the mapped data
-  var firstSeries = chart.line(firstSeriesData)
-  firstSeries.name("A")
-
-  // create the second series with the mapped data
-  var secondSeries = chart.line(secondSeriesData)
-  secondSeries.name("B")
-
-  // turn the legend on
-  chart.legend().enabled(false)
-
-  // set the container id for the line chart
-  chart.container("chart_panel")
-
-  // draw the line chart
-  chart.draw()
-  modal.classList.toggle("toggle-modal")
-})
-
-closeModal.addEventListener("click", () => {
-  modal.classList.toggle("toggle-modal")
+// Checkout hide and show function
+closeCheckout.addEventListener("click", () => {
+  checkout.style.display = "none"
+  withDrawPanel.style.width = "100%"
 })
