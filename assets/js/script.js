47946aeea2678005f0cfaa39ef562d1c9853ac78
webmiraclepro <webmiraclepro@gmail.com>
2022-01-23 21:00:31 +0800

    vanila-update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+// Button element variables
+let openChatBox = document.getElementById("open_chatbox");
+let closeChatBox = document.getElementById("close_chatbox");
+let openSideBar = document.getElementById("btn_sidebar");
+let openMenu = document.getElementById("open_menu");
+
+// Component variable
+let chatBox = document.querySelector(".chat-box");
+let main = document.querySelector(".main");
+let sideBar = document.querySelector(".sidebar");
+let vMenu = document.querySelector(".v-menu");
+
 // Chat send textarea auto resize function
 const tx = document.getElementsByTagName("textarea");
 for (let i = 0; i < tx.length; i++) {
@@ -10,50 +22,23 @@ function OnInput() {
 }
 
 // Chatbox hide and show function
-let openChatBox = document.getElementById("open_chatbox");
-let closeChatBox = document.getElementById("close_chatbox");
-let chatBox = document.querySelector(".chat-box");
-let main = document.querySelector(".main");
-let arrow = document.getElementById("btn_arrow");
-let arrowBtn = document.querySelector("#btn_arrow");
-let arrowIcon = document.getElementById("arrow_icon");
-let openSideBar = document.getElementById("btn_sidebar");
-let sideBar = document.querySelector(".sidebar");
-let openMenu = document.getElementById("open_menu");
-let vMenu = document.querySelector(".v-menu");
-
-// openChatBox.addEventListener("click", () => {
-//   if (getComputedStyle(chatBox).display == "flex") {
-//     chatBoxClose()
-//   } else {
-//     chatBox.style.display = "flex"
-//     main.style.width = "calc(100% - 440px)"
-//   }
-// })
-
-// closeChatBox.addEventListener("click", chatBoxClose)
-
-// function chatBoxClose() {
-//   chatBox.style.display = "none"
-//   main.style.width = "calc(100% - 118px)"
-//   arrowBtn.style.display = "none"
-// }
-
 openChatBox.addEventListener("click", () => {
-  if (getComputedStyle(chatBox).right == "-416px") {
-    chatBox.style.right = "1rem";
+  if (getComputedStyle(chatBox).right == "0px") {
+    chatBox.style.right = "-30rem";
+    if (window.innerWidth < 680) main.style.width = "calc(100% - 48px)";
+    else main.style.width = "calc(100% - 118px)";
   } else {
-    chatBox.style.right = "-26rem";
+    chatBox.style.right = "0rem";
+    if (window.innerWidth > 1366) main.style.width = "calc(100% - 440px)";
+    else if (window.innerWidth < 680) main.style.width = "calc(100% - 48px)";
+    else main.style.width = "calc(100% - 118px)";
   }
-  if (window.innerWidth > 798) main.style.width = "calc(100% - 440px)";
-  else main.style.width = "calc(100% - 48px)";
 });
 
 closeChatBox.addEventListener("click", () => {
-  chatBox.style.right = "-26rem";
-  main.style.width = "calc(100% - 118px)";
-  if (window.innerWidth > 798) main.style.width = "calc(100% - 440px)";
-  else main.style.width = "calc(100% - 48px)";
+  chatBox.style.right = "-30rem";
+  if (window.innerWidth < 680) main.style.width = "calc(100% - 48px)";
+  else main.style.width = "calc(100% - 118px)";
 });
 
 // Sidebar hide and show function
@@ -74,3 +59,5 @@ openMenu.addEventListener("click", () => {
     vMenu.style.display = "none";
   }
 });
+
+// Checkout
